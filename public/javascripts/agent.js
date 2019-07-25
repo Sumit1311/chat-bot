6b41638ceade7049624c7e2acdc3d09b684602f1
Sumit <sumittoshniwal92@gmail.com>
2019-07-25 06:42:17 +0530

    Adding a bare minimum functionality

--- public/javascripts/agent.js
+++ public/javascripts/agent.js
@@ -2,22 +2,53 @@
  * Created by geek on 23/7/19.
  */
 $(
-    function(){
+    function () {
+        function onClickHandler(event) {
+            var tr = $(this);
+            var id = tr.attr("id");
+            console.log("Join Customer");
+            socket.emit("join-customer", {
+                id: id
+            });
+            //$("#"id).remove();
+
+        }
+
         "use strict";
         var socket = io();
         socket.on('connect', () => {
             socket.emit('agent login');
+            $("#customer-list").empty();
         });
-        $('form').submit(function(){
-
+        $('form').submit(function () {
             socket.emit('message', $('#m').val());
             $('#messages').append($('<li>').addClass('client-message').text($('#m').val()));
             $('#m').val('');
             return false;
         });
-        socket.on('message', function(msg){
+
+        socket.on('message', function (msg) {
             $('#messages').append($('<li>').addClass('server-message').text(msg));
             window.scrollTo(0, document.body.scrollHeight);
+        });
+        socket.on('customer-list', function (clients) {
+            $("#customer-list").append("<tbody></tbody>");
+            for (var i in clients) {
+                if($("#" + clients[i].id).length == 0) {
+                    $("#customer-list > tbody").append("<tr id='" + clients[i].id + "'><td><a>" + clients[i].id + "</a></td></tr>");
+                    $("#" + clients[i].id).click(onClickHandler);
+                }
+            }
+        });
+        socket.on('add-customer', function (client) {
+                if($("#" + client.id).length == 0) {
+                    $("#customer-list > tbody").append("<tr id='" + client.id + "'><td><a>" + client.id + "</a></td></tr>");
+                    $("#" + client.id).click(onClickHandler);
+                }
+        });
+        socket.on('remove-customer', function (client) {
+            //console.log("got remove customer", client);
+            $("#" + client.id).remove();
         });
     }
 )
--- websocket.js
+++ websocket.js
@@ -5,20 +5,43 @@
 var io = require('socket.io')();
 let Customer = require('./lib/Customer.js');
 let Agent = require('./lib/Agent.js');
-
-var customer = null;
+let CustomerQueue = require('./lib/CustomerQueue.js');
+var customers = new CustomerQueue();
+customers.registerAddCustomer((client) => {
+    io.to('agent-room').emit('add-customer', client);
+});
+customers.registerRemoveCustomer((client) => {
+    io.to('agent-room').emit('remove-customer', client);
+});
+//var customer = null;
 io.on('connection', (socket)  => {
     "use strict";
-    console.log("Client Connected ",socket.id);
-
+    //console.log("Client Connected ",socket.id);
     socket.on("agent login", () => {
+        //let agent = new Agent(socket, findCustomer);
         let agent = new Agent(socket);
-        agent.attend(customer);
+        agent.sendCustomerList(customers.getList());
+        agent.onJoinCustomer((client) => {
+            //console.log(client);
+            let customer = customers.getCustomerById(client.id);
+            agent.attend(customer);
+            if(customer != null){
+                customers.removeCustomer(customer);
+            }
+        });
+        //agent.attend();
     });
+
     socket.on("customer login", () => {
-        customer = new Customer(socket);
+        let customer = new Customer(socket);
+        customers.addCustomer(customer);
+        customer.sendMessage("Your waiting queue number is "+customers.getWaitCount(customer));
+        customer.onDisconnect(() => {
+            customers.removeCustomer(customer);
+        });
         //customer.registerEventHandlers();
     });
+
     /*socket.on('chat message', function (data) {
         console.log(data);
         socket.emit('chat message', "This is server");
@@ -27,5 +50,15 @@ io.on('connection', (socket)  => {
         console.log("Client disconnected", socket.id);
     })*/
 });
+
+function findCustomer(){
+    "use strict";
+    if(customers.isEmpty()){
+        return null;
+    } else {
+        let customer = customers.getNextCustomer();
+        return customer;
+    }
+}
 
 module.exports = io;
